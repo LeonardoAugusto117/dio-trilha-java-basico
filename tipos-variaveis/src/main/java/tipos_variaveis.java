d549654dfbcb839ca0e200ce164c888e645fda67
LeonardoAugusto117 <leonardo.augusto@linkedby.com.br>
2025-03-17 19:30:09 -0300

    subindo novas aulas

--- tipos-variaveis/src/main/java/tipos_variaveis.java
+++ tipos-variaveis/src/main/java/tipos_variaveis.java
@@ -1,2 +1,19 @@
 public class tipos_variaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
 }
